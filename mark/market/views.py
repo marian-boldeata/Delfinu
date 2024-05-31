f8540a58e10f99860e6b2cbe05787dcb728e1964
marian-boldeata <m.boldeata@gmail.com>
2024-05-31 21:25:35 +0300

    added html files - home navbar base, added css and banner

--- mark/market/views.py
+++ mark/market/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This will be the main page")
+    return render(request, "market/home.html")
 
 
