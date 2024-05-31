09099fc0a01514b28646d6265fb3bcf2e5058b23
marian-boldeata <m.boldeata@gmail.com>
2024-05-31 20:16:17 +0300

    added MEDIA_ROOT and MEDIA_URL for img processing, added more complexity to models - description,price,quantity and images

--- mark/market/models.py
+++ mark/market/models.py
@@ -4,12 +4,19 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-
     def __str__(self):
         return self.name
+
+
+
+
 class Product(models.Model):
     nume_produs = models.CharField(max_length=30)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    descriere = models.CharField(max_length=200,default="...")
+    pret = models.IntegerField(null=False,default=1)
+    cantitate = models.IntegerField(default=1)
+    imagine_produs = models.ImageField(upload_to="imagini_produs/", default="mark/no img available")
 
     def __str__(self):
         return self.nume_produs
